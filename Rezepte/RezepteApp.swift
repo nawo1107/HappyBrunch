df0197001215f0f274d5e243e405376712e32779
Natalie Wolf <nataliewolf@gmx.net>
2020-09-26 14:19:19 +0200

    Initial Commit

---
+++ Rezepte/RezepteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RezepteApp.swift
+//  Rezepte
+//
+//  Created by Natalie Wolf on 26.09.20.
+//
+
+import SwiftUI
+
+@main
+struct RezepteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
